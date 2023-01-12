29e33b2035e6a9f3cb3b7979d55da909c7f6d04a
wathika <71040609+wathika-eng@users.noreply.github.com>
2023-01-13 00:14:09 +0300

    Update phone.js

--- phone.js
+++ phone.js
@@ -1,7 +1,7 @@
 
    const phoneInputField = document.querySelector("#phone");
    const phoneInput = window.intlTelInput(phoneInputField, {
-    preferredCountries: ["ke"],
+    preferredCountries: ["ke", "ksa", "ug", "tz"],
     
      utilsScript:
        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
