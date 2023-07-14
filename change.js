c3eb3061d073c78edda3fba7ce23d9c992900cbf
wathika-eng <wathika02@gmail.com>
2023-07-14 12:09:14 +0300

    additions

--- change.js
+++ change.js
@@ -1,18 +1,26 @@
 let signupBtn = document.getElementById("signupBtn");
 let loginBtn = document.getElementById("loginBtn");
-let namefield = document.getElementById("namefield");
+var names = document.getElementById("names");
+var num = document.getElementById("num");
+var mail = document.getElementById("mail");
 let title = document.getElementById("title");
 
 
 loginBtn.onclick = function(){
-    namefield.style.maxHeight = "0px";
+    document.getElementById("names").style.display = "none";
+    document.getElementById("num").style.display = "none";
+    document.getElementById("mail").style.display = "none";
+    
     title.innerHTML = "Log-In";
     signupBtn.classList.add("disable");
     loginBtn.classList.remove("disable");
 }
 
 signupBtn.onclick = function(){
-    namefield.style.maxHeight = "60px";
+    document.getElementById("names").style.display = "flex";
+    document.getElementById("mail").style.display = "flex";
+    document.getElementById("num").style.display = "flex";
+    
     title.innerHTML = "Sign-Up";
     loginBtn.classList.add("disable");
     signupBtn.classList.remove("disable");
